db27e91963c8150fdd65a824af60385a6f25ea06
qnguyen210 <qnguy120@my.centennialcollege.ca>
2025-12-01 23:33:53 -0500

    Add Vitest and React Testing Library configuration/ create unit test for login component inputs and buttion/ add setupTests.js for jest-dom

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  },
-});
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js'
+  }
+})
